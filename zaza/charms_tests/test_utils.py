35e6f8b858539f3150afc7867e8ca3c6861f1a79
Liam Young <liam.young@canonical.com>
2018-03-21 14:45:03 +0000

    Update decorator

--- zaza/charms_tests/test_utils.py
+++ zaza/charms_tests/test_utils.py
@@ -3,8 +3,13 @@ import zaza.model
 
 
 def skipIfNotHA(service_name):
-    """Skip test if there is 1 or less units of a service"""
-    if len(zaza.model.unit_ips(service_name)) > 1:
-        return lambda func: func
-    return unittest.skip(
-        "Skipping HA test for non-ha service {}".format(service_name))
+    def _skipIfNotHA_inner_1(f):
+        def _skipIfNotHA_inner_2(*args, **kwargs):
+            if len(zaza.model.unit_ips(service_name)) > 1:
+                return f(*args, **kwargs)
+            else:
+                return unittest.skip("Skipping HA test for non-ha service {}".format(service_name))
+        return _skipIfNotHA_inner_2
+
+    return _skipIfNotHA_inner_1
+
