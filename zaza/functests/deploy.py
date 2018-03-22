20317d58491136f7469dcc17de27c5c3f3b9e51c
Liam Young <liam.young@canonical.com>
2018-03-22 09:58:38 +0000

    Switch to str representation of test classes

--- zaza/functests/deploy.py
+++ zaza/functests/deploy.py
@@ -1,14 +1,17 @@
 import argparse
 import datetime
-import unittest
+import importlib
 import logging
 import os
 import subprocess
 import sys
+import unittest
+import yaml
 
 import juju_wait
 
 BUNDLE_DIR = "./tests/bundles/"
+DEFAULT_TEST_CONFIG = "./tests/tests.yaml"
 
 def deploy_bundle(bundle, model):
     logging.info("Deploying bundle {}".format(bundle))
@@ -18,36 +21,42 @@ def add_model(model_name):
     logging.info("Adding model {}".format(model_name))
     subprocess.check_call(['juju', 'add-model', model_name])
 
-def _run_tests():
-    for testcase in tests.TESTS:
+def get_test_class(class_str):
+    test_module_name = '.'.join(class_str.split('.')[:-1])
+    test_class_name = class_str.split('.')[-1]
+    test_module = importlib.import_module(test_module_name)
+    return getattr(test_module, test_class_name)
+
+def run_test_list(tests):
+    for _testcase in tests:
+        testcase = get_test_class(_testcase)
         suite = unittest.TestLoader().loadTestsFromTestCase(testcase)
         test_result = unittest.TextTestRunner(verbosity=2).run(suite)
         assert test_result.wasSuccessful(), "Test run failed"
 
-def load_charm_test_config(test_dir):
-    sys.path.append(test_dir)
-    import tests
-
-def _deploy():
-    for t in tests.GATE_BUNDLES:
+def deploy():
+    test_config = get_test_config()
+    for t in test_config['gate_bundles']:
         timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        model_name = '{}{}{}'.format(tests.CHARM_NAME, t, timestamp)
+        model_name = '{}{}{}'.format(test_config['charm_name'], t, timestamp)
         add_model(model_name)
-        deploy_bundle(os.path.join(BUNDLE_DIR, '{}.yaml'.format(t)), model_name)
+        deploy_bundle(
+            os.path.join(BUNDLE_DIR, '{}.yaml'.format(t)),
+            model_name)
         logging.info("Waiting for environment to settle")
         juju_wait.wait()
-        _run_tests()
+        run_test_list(test_config['tests'])
 
-def run(f):
+def get_test_config():
+    with open(DEFAULT_TEST_CONFIG, 'r') as stream:
+        return yaml.load(stream)
+
+def run_tests():
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
-    parser.add_argument("test_dir", help="Test directory")
+    parser.add_argument('-t','--tests', nargs='+',
+                        help='Space sperated list of test classes',
+                        required=False)
     args = parser.parse_args()
-    load_charm_test_config(args.test_dir)
-    f()
- 
-def deploy():
-    run(_deploy)
- 
-def run_tests():
-    run(_run_tests)
+    tests = args.tests or get_test_config()['tests']
+    run_test_list(tests)
